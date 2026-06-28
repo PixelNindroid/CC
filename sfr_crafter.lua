0555d228286580949cb0b5c2a1b96b1b4d67793d
PixelNindroid <bramvandormael@gmail.com>
2026-06-28 19:21:29 +0200

    Auto-save: sfr_crafter.lua

---
+++ sfr_crafter.lua
@@ -0,0 +1,22 @@
+local r = require('libs.redlib')
+
+peripheral.find("modem", rednet.open)
+--rednet.host('SFR_CRAFTER', 'sfr_crafter')
+
+print('Hosting..')
+rednet.host('SFR_CRAFTER', 'sfr_crafter')
+print('Done!')
+
+
+local function craft()
+    turtle.craft()
+    print('Craft!')
+end
+print('jee')
+
+while true do
+    local id, msg = rednet.receive('ACTION')
+    r.listen(id, msg, 'ACTION', {
+        craft = craft
+    })
+end
