43c81aeaf7b5a40f52aff6527cb027d2a71fbd74
PixelNindroid <bramvandormael@gmail.com>
2026-06-28 18:50:47 +0200

    Auto-save: grab.lua

--- grab.lua
+++ grab.lua
@@ -48,7 +48,7 @@ function grab.unserializeJSON(path)
 end
 
 local function getGitRepo(fileName)
-    local link = GIT_REPO_URL .. fileName
+    local link = GIT_REPO_URL .. CATEGORIES[fileName] or '' .. '/' .. fileName
     local request
 
     while not request do
