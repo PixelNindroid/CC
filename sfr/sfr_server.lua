753b29288b35f6da1b0789f0ee8ccaec5bf324a2
PixelNindroid <bramvandormael@gmail.com>
2026-07-02 13:01:36 +0200

    Auto-save: sfr_server.lua

--- sfr/sfr_server.lua
+++ sfr/sfr_server.lua
@@ -222,7 +222,9 @@ local function getItemDetails(ir)
     
     local details = {}
     for _, id in ipairs(itemIDs) do
-        local describe = ir.describe('item', id)
+        local _, describe = pcall(function() 
+            return ir.describe('item', id) 
+        end)
 
         details[id] = describe and {
             displayName = describe.displayName or id, --string.gsub(describe.displayName, "§.", ""),
