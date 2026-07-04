b0976514f46f03e18bacb43b59fe2408ba680229
PixelNindroid <bramvandormael@gmail.com>
2026-07-04 19:52:54 +0200

    Auto-save: grab.lua

--- grab.lua
+++ grab.lua
@@ -152,7 +152,8 @@ if not settings.get('grab.grab_scripts_on_startup') then
 end
 
 if not settings.get('grab.main') then 
-    clear()
+    term.clear()
+    term.setCursorPos(1, 1)
     print('Welcome to grab installer\174!\n')
     print('Select the Main program:')
     local mains = {}
