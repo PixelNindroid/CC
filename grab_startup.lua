9a1bd4cf1605eabea0751204c70aaee8f29bba27
PixelNindroid <bramvandormael@gmail.com>
2026-07-04 19:38:47 +0200

    Auto-save: grab_startup.lua

--- grab_startup.lua
+++ grab_startup.lua
@@ -4,9 +4,11 @@ local main = '%s'
 term.clear()
 
 settings.load()
-if settings.get('grab_scripts_on_startup') then
+if settings.get('grab.grab_scripts_on_startup') then
     grab.grabAll(main)
 end
 
-print('Running ' .. main .. '..\n')
-shell.run(main)
+if settings.get('grab.run_main_on_startup') then
+    print('Running ' .. main .. '..\n')
+    shell.run(main)
+end
